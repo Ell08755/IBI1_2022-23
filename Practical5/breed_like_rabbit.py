0b65841864e181d0b03c0f8a82a03ee6c327ff3b
unknown <siyuan.22@intl.zju.edu.cn>
2023-03-16 20:10:51 +0800

    fulfill rabbit_breed's pseudocode

--- Practical5/breed_like_rabbit.py
+++ Practical5/breed_like_rabbit.py
@@ -1,9 +1,10 @@
 #rabbit generation
-#ge is generation number, n is the rabbit ammount
+#ge is generation number, n is the rabbit amount, initialse the numbers
 ge=1
 n=2
-#to test the generation with loop
+#to test the generation with loop, store the generation number and amount
 while n<=100:
  n=n*2
  ge=ge+1
+#print conclusion
 print ('The number of generations required to exceed 100 rabbits is '+ str(ge))
