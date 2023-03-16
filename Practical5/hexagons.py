e3254ad5c33216a9db3111c8c246464c57917d54
unknown <siyuan.22@intl.zju.edu.cn>
2023-03-16 20:13:08 +0800

    fulfill hexagons' pseudocode

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,6 +1,8 @@
 #use 'for' loop, here create the variables needed
 for i in range (1,6):
+#the hexagonal number needed
  h=2*i*(2*i-1)/2
+#print conclusions
  print ('hexagonal number',i,'is',h)
 
 
