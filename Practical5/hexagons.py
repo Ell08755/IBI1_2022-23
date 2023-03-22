4ba4162b1716b75ec8e24833db71b5e026e5866a
Ell08755 <127209140+Ell08755@users.noreply.github.com>
2023-03-23 01:11:57 +0800

    Update pseudocode

--- Practical5/hexagons.py
+++ Practical5/hexagons.py
@@ -1,8 +1,8 @@
-#use 'for' loop, here create the variables needed
+#use 'for' loop and hexanumber formular, here create the variables needed
 for i in range (1,6):
 #the hexagonal number needed
  h=2*i*(2*i-1)/2
-#print conclusions
+#print conclusions in every loops
  print ('hexagonal number',i,'is',h)
 
 
