df2b94ccbcadf130f1ca3d078c60dec8f1bacd3b
Ell08755 <127209140+Ell08755@users.noreply.github.com>
2023-03-23 01:09:55 +0800

    Update pseudocode

--- Practical5/breed_like_rabbit.py
+++ Practical5/breed_like_rabbit.py
@@ -1,8 +1,8 @@
 #rabbit generation
-#ge is generation number, n is the rabbit amount, initialse the numbers
-ge=1
-n=2
-#to test the generation with loop, store the generation number and amount
+#creat two variables to store generation number and rabbit amount, initialse the numbers
+ge=1 #ge means generation
+n=2 #n means rabbit amount
+#to test the generation with while loop, double every generation, store the generation number and amount
 while n<=100:
  n=n*2
  ge=ge+1
