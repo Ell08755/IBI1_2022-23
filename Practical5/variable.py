e6c2781ba417d5796af765766eb559edeb57fa15
Ell08755 <127209140+Ell08755@users.noreply.github.com>
2023-03-23 01:15:48 +0800

    Update pseudocode

--- Practical5/variable.py
+++ Practical5/variable.py
@@ -1,4 +1,5 @@
 #Distance comparision
+#create variables to store the distances
 #a is distance to Edinburgh
 a=-3.19
 #b is distance to Los Angeles
@@ -9,7 +10,7 @@ c=116.39
 d=a-b
 #e is distace from Haining to Edinburgh
 e=c-a
-#comparison
+#comparision, if d<e, then print 'The distance between LA and Edinburgh is shorter than distance between Haining and Edinburgh', and vice versa except for same distance situation.
 if d<e:
  print ('The distance between LA and Edinburgh is shorter than distance between Haining and Edinburgh')
 elif d>e:
@@ -17,11 +18,12 @@ elif d>e:
 else: 
  print ('The distance is same.')
 #Booleans
-#set up the origin variables
+#set up the origin variables, X is True, Y is False
 X=True
 Y=False
-#define W and Z
+#define W and Z, W is X and Y, Z is X or Y.
 W=X and Y
 Z=X or Y
+#print the result
 print ('W is',W,'and Z is',Z)
 
