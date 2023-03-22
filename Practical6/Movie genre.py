11c3f3a4381a01c99d4712373dfc0db1f9910211
Ell08755 <127209140+Ell08755@users.noreply.github.com>
2023-03-23 01:06:53 +0800

    Update Movie genre.py

--- Practical6/Movie genre.py
+++ Practical6/Movie genre.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 my_dict = {}
 Movie_genres = {'Comedy':73,'Action':42,'Romance':38,'Fantasy':28,'Science-Fiction':22,'Horror':19,'Crime':18,'Documentary':12,'History':8,'War':7}
 print('Movie genres dictionary', Movie_genres)
-# Draw piechart from dictionary, set up as equal, percent number, show the piechart
+# Draw piechart from dictionary, set up as equal(shape), percent number, show the piechart
 plt.pie(Movie_genres.values(),labels=Movie_genres.keys(),autopct='%1.1f%%')
 plt.axis('equal')
 plt.show()
-#test input, the variable name is GenrQ, and use 'Comedy' as example for practical task
+# test input, the variable name is GenrQ, and use 'Comedy' as example for practical task
 # fetch the value by key, and print it
 GenrQ=Movie_genres['Comedy']
 print('Comedy favorite students number:', GenrQ)
